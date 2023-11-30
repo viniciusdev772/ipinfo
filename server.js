2cad664446602c350efd03843dd0a2e1cef18302
viniciusdev772 <viniciusdev37@gmail.com>
2023-11-29 21:27:34 -0300

    d

--- server.js
+++ server.js
@@ -11,11 +11,12 @@ app.get('/', (req, res) => {
       console.error(err);
       res.status(500).send('Erro ao obter informações do IP');
     } else {
-      res.json(informacoesIp);
+      //res.json(informacoesIp);
+      res.status(500).send('Erro ao obter informações do IP');
     }
   });
 });
 
-app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`);
-});
+app.listen(port, "0.0.0.0", function () {
+    console.log(`Servidor rodando em http://0.0.0.0:${port}`);
+  });
