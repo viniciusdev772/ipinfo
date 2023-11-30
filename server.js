b7d96bba69ac84cd7918ec24419cc8d7c7f26cb4
viniciusdev772 <viniciusdev37@gmail.com>
2023-11-30 20:57:57 -0300

    dhrjhrjrf

--- server.js
+++ server.js
@@ -42,31 +42,30 @@ const { XVDL } = require("xvdl");
 app.get('/xvideos/:xvideosLink', async (req, res) => {
   const targetLink = req.params.xvideosLink;
 
-  XVDL.getInfo(targetLink).then(async (inf) => {
-    console.log(targetLink);
-    
-    const filePath = "caminho/do/arquivo";  // Defina o caminho adequado
-    
-    const downloadLink = await XVDL.download(targetLink, { type: "hq" });
-    
-    // Criar stream de escrita
-    const writeStream = fs.createWriteStream(filePath);
-    
-    // Redirecionar o conteúdo do download para o stream de escrita
-    downloadLink.pipe(writeStream);
+  try {
+    const inf = await XVDL.getInfo(targetLink);
 
-    // Quando o download estiver completo, enviar a resposta JSON com o link de download
-    writeStream.on('finish', () => {
+    const downloadLink = await XVDL.download(targetLink, { type: "hq" });
+
+    // Em vez de usar fs.createWriteStream, redirecione o conteúdo diretamente para a resposta
+    downloadLink.pipe(res);
+
+    // Quando o download estiver completo, envie uma resposta JSON com o link de download
+    res.on('finish', () => {
       const jsonResponse = {
         statusCode: 200,
         status: "sucesso",
         link: inf,
-        downloadLink: targetLink,  // Atualize o caminho conforme necessário
+        downloadLink: `caminho/do/arquivo/${targetLink}`,  // Atualize o caminho conforme necessário
       };
       res.json(jsonResponse);
     });
-  });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Erro ao obter informações do link Xvideos.' });
+  }
 });
+
 
 
 // Rota para obter informações do IP padrão
