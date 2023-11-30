45a08043ee072b1224664f0719e6ee5b1b66dd5b
viniciusdev772 <viniciusdev37@gmail.com>
2023-11-29 21:30:16 -0300

    f

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const ipinfo = require('ipinfo');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   const enderecoIp = req.ip; // Pode precisar usar req.clientIp se estiver usando o middleware request-ip
